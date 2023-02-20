02b27565fea0068925c61907fa478b486414af60
Yoel Moya Carmona <124745119+YoelMoya99@users.noreply.github.com>
2023-02-20 08:16:49 -0600

    Problemas con git
    Se subieron archivos que no tenian que ver

--- host/gato.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-'''
-Inicio del codigo para gato.
-'''
--- host/hostgui.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import socket
-import gi
-
-gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
-
-host = "10.0.2.16" 
-port = 65432 
